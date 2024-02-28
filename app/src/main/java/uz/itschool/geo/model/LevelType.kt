6bb28fd5fea6d7f879babe1555dd79a188049090
behruzbek2Gulomov <120156394+behruzbek2Gulomov@users.noreply.github.com>
2024-02-28 13:13:00 +0500

    3days after deadline

--- app/src/main/java/uz/itschool/geo/model/LevelType.kt
+++ app/src/main/java/uz/itschool/geo/model/LevelType.kt
@@ -5,7 +5,9 @@ import uz.itschool.geo.R
 enum class LevelType(val text:String, val image:Int) {
     STUDENT("Student", R.drawable.student),
     TOURIST("Tourist", R.drawable.traveller),
-    DRIVER("Driver", R.drawable.coin),
-    CAPTAIN("Captain", R.drawable.coin),
-    TEACHER("Teacher", R.drawable.coin),
+    DRIVER("Driver", R.drawable.driver),
+    CAPTAIN("Captain", R.drawable.captain),
+    TEACHER("Teacher", R.drawable.teacher),
+    SCIENTIST("Scientist",R.drawable.chemistry),
+    VIRGIN_OPENER("Land Opener",R.drawable.virgin_opener)
 }
